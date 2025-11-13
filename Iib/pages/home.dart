4f94b2c99597ac2fed8e6ce9329cac9d1b93e287
Dratiss <eduardomichelsouzabarros@gmail.com>
2025-11-13 15:18:10 -0400

    Implement initial HomePage layout in Flutter

--- Iib/pages/home.dart
+++ Iib/pages/home.dart
@@ -1,1 +1,19 @@
-// Página inicial da PromoHard (arquivo temporário)
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PromoHard"),
+        backgroundColor: Color(0xFF7B1FA2),
+      ),
+      body: Center(
+        child: Text(
+          "Carregando promoções...",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
